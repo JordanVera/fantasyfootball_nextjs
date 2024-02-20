db64256154fc0f90aedbf6ff5a1752cf2a023747
Jordan Vera <verawebdevelopment@gmail.com>
2024-02-19 20:54:23 -0600

    Add TeamLogosSlider component

---
+++ components/TeamLogosSlider.jsx
@@ -0,0 +1,51 @@
+const TeamLogosSlider = () => {
+  return (
+    <section
+      className="logoBanner flex flex-row gap-5"
+      style={{
+        paddingTop: '12px',
+        paddingBottom: '12px',
+        overflow: 'auto',
+        whiteSpace: 'nowrap',
+      }}
+    >
+      <img className="h-14 w-14" src="/images/teamLogos/bears.png" alt="" />
+      <img className="h-14 w-14" src="/images/teamLogos/panthers.png" alt="" />
+      <img className="h-14 w-14" src="/images/teamLogos/saints.png" alt="" />
+      <img className="h-14 w-14" src="/images/teamLogos/texans.png" alt="" />
+      <img className="h-14 w-14" src="/images/teamLogos/ravens.png" alt="" />
+      <img
+        className="h-14 w-14"
+        src="/images/teamLogos/footballTeam.png"
+        alt=""
+      />
+      <img className="h-14 w-14" src="/images/teamLogos/chiefs.png" alt="" />
+      <img className="h-14 w-14" src="/images/teamLogos/buccs.png" alt="" />
+      <img className="h-14 w-14" src="/images/teamLogos/jaguars.png" alt="" />
+      <img className="h-14 w-14" src="/images/teamLogos/raiders.png" alt="" />
+      <img className="h-14 w-14" src="/images/teamLogos/patriots.png" alt="" />
+      <img className="h-14 w-14" src="/images/teamLogos/rams.png" alt="" />
+      <img className="h-14 w-14" src="/images/teamLogos/vikings.png" alt="" />
+      <img className="h-14 w-14" src="/images/teamLogos/titans.png" alt="" />
+      <img className="h-14 w-14" src="/images/teamLogos/seahawks.png" alt="" />
+      <img className="h-14 w-14" src="/images/teamLogos/packers.png" alt="" />
+      <img className="h-14 w-14" src="/images/teamLogos/falcons.png" alt="" />
+      <img className="h-14 w-14" src="/images/teamLogos/eagles.png" alt="" />
+      <img className="h-14 w-14" src="/images/teamLogos/dallas.png" alt="" />
+      <img className="h-14 w-14" src="/images/teamLogos/dolphins.png" alt="" />
+      <img className="h-14 w-14" src="/images/teamLogos/cols.png" alt="" />
+      <img className="h-14 w-14" src="/images/teamLogos/jets.png" alt="" />
+      <img className="h-14 w-14" src="/images/teamLogos/niners.png" alt="" />
+      <img className="h-14 w-14" src="/images/teamLogos/giants.png" alt="" />
+      <img className="h-14 w-14" src="/images/teamLogos/lions.png" alt="" />
+      <img className="h-14 w-14" src="/images/teamLogos/cleveland.png" alt="" />
+      <img className="h-14 w-14" src="/images/teamLogos/chargers.png" alt="" />
+      <img className="h-14 w-14" src="/images/teamLogos/cardinals.png" alt="" />
+      <img className="h-14 w-14" src="/images/teamLogos/bills.png" alt="" />
+      <img className="h-14 w-14" src="/images/teamLogos/bengals.png" alt="" />
+      <img className="h-14 w-14" src="/images/teamLogos/steelers.png" alt="" />
+      <img className="h-14 w-14" src="/images/teamLogos/broncos.png" alt="" />
+    </section>
+  );
+};
+export default TeamLogosSlider;
