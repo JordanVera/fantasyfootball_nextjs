efa57f763e8e0780f0e25b0ff2a327d22c5de303
Jordan Vera <verawebdevelopment@gmail.com>
2024-02-20 21:17:55 -0600

    Add avatar and user name to Main Sidebar

--- components/Main_Sidebar.jsx
+++ components/Main_Sidebar.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
-import { IconButton } from '@material-tailwind/react';
+import { IconButton, Avatar } from '@material-tailwind/react';
 import MenuIcon from '@mui/icons-material/Menu';
 import Link from 'next/link';
 
 import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
 import SpaceDashboardIcon from '@mui/icons-material/SpaceDashboard';
+import { useSession } from 'next-auth/react';
 
 function Main_Sidebar() {
+  const { data: session, status } = useSession();
   const [isCollapsed, setIsCollapsed] = useState(true);
 
   const toggleSidebar = () => {
@@ -15,26 +17,45 @@ function Main_Sidebar() {
 
   return (
     <div
-      className={`transition-all duration-200 ease-in-out flex flex-col p-5 gap-3 ${
-        isCollapsed ? 'w-16 items-center ' : 'w-48 justify-start'
+      className={`transition-all duration-200 ease-in-out flex flex-col p-5 h-full ${
+        isCollapsed ? 'w-18 items-center ' : 'w-48 justify-start'
       }`}
     >
       <IconButton onClick={toggleSidebar}>
         <MenuIcon className="text-white" />
       </IconButton>
 
-      <Link href="/" className="flex items-center">
+      <Link
+        href="/"
+        className="flex items-center hover:bg-gray-900 p-2 rounded-lg"
+      >
         <HomeOutlinedIcon />
         <span className={`ml-3 ${isCollapsed ? 'hidden' : 'inline'}`}>
           Home
         </span>
       </Link>
-      <Link href="/dashboard" className="flex items-center">
+
+      <Link
+        href="/dashboard"
+        className="flex items-center hover:bg-gray-900 p-2 rounded-lg"
+      >
         <SpaceDashboardIcon />
         <span className={`ml-3 ${isCollapsed ? 'hidden' : 'inline'}`}>
           Dashboard
         </span>
       </Link>
+
+      <div className="mt-auto">
+        <Link
+          href="/dashboard"
+          className="flex items-center hover:bg-gray-900 p-2 rounded-lg"
+        >
+          <Avatar src={session?.user?.image} className="h-8 w-8" />
+          <span className={`ml-3 ${isCollapsed ? 'hidden' : 'inline'}`}>
+            {`${session?.user?.name}`}
+          </span>
+        </Link>
+      </div>
     </div>
   );
 }
