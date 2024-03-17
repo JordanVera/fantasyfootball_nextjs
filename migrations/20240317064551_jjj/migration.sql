fd5d6790a2a4f3dd4ff04d161be171db4d32f93b
Jordan Vera <verawebdevelopment@gmail.com>
2024-03-17 01:50:54 -0500

    Remove unused columns and add unique constraint to username field

--- migrations/20240317064551_jjj/migration.sql
+++ migrations/20240317064551_jjj/migration.sql
@@ -1,4 +1,5 @@
 -- AlterTable
 ALTER TABLE `users` ADD COLUMN `firstname` VARCHAR(191) NULL,
     ADD COLUMN `lastname` VARCHAR(191) NULL,
-    ADD COLUMN `password` VARCHAR(191) NULL;
+    ADD COLUMN `password` VARCHAR(191) NULL,
+    ADD COLUMN `username` VARCHAR(191) NULL;
