02345f81034722c6d867388bf83d2a6dd396eb70
Jordan Vera <verawebdevelopment@gmail.com>
2024-03-17 22:55:14 -0500

    Change color prop value from "indigo" to "blue" in RegistrationDialog component

--- components/dialogs/RegistrationDialog.jsx
+++ components/dialogs/RegistrationDialog.jsx
@@ -97,7 +97,7 @@ export default function RegistrationDialog() {
             <Select
               label="number of entries"
               className="capitalize text-primary"
-              color="indigo"
+              color="blue"
               variant="standard"
               onChange={(val) => setNumberOfEntries(val)}
             >
