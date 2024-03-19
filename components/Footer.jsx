562d3fa0ebf61af4fcac03d73fbdda5388d26cbb
Jordan Vera <verawebdevelopment@gmail.com>
2024-03-18 23:47:37 -0500

    Update Footer component styling

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -5,7 +5,7 @@ import Logo from './Logo';
 export const Footer = () => {
   return (
     <footer className="bg-white shadow dark:bg-gray-900 mt-20">
-      <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+      <div className="w-full  mx-auto p-10 md:py-8">
         <div className="sm:flex sm:items-center sm:justify-between">
           <Link
             href="/"
