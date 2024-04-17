762d754087478498409c385119c778b99870c56e
Jordan Vera <verawebdevelopment@gmail.com>
2024-04-16 21:25:12 -0500

    Refactor SignupCTA component to replace Link with div for better accessibility and update layout and styles

--- components/homepage/SignupCTA.jsx
+++ components/homepage/SignupCTA.jsx
@@ -16,50 +16,51 @@ const SignupCTA = () => {
   });
 
   return (
-    <div
-      ref={ref}
-      href={'/login'}
-      className="hover:scale-105 duration-300 cursor-pointer"
-    >
-      <motion.div
-        className="bg-black rounded-lg flex flex-col-reverse md:flex-row m-5"
-        initial={{ y: '100%', opacity: 0 }}
-        animate={{ y: inView ? '0%' : '100%', opacity: inView ? 1 : 0 }}
-        transition={{ duration: 0.7, ease: 'easeIn' }}
+    <div ref={ref}>
+      <Link
+        href={'/login'}
+        className="hover:scale-105 duration-300 cursor-pointer"
       >
-        <div className="flex flex-col gap-5 p-10 rounded-b-lg lg:rounded-l-lg md:rounded-br-none w-full lg:w-1/4 bg-[#ffff3f]">
-          <div className="flex flex-row justify-center border bg-white text-black rounded-full w-[60px] px-2 py-1">
-            <p>Hello</p>
+        <motion.div
+          className="bg-black rounded-lg flex flex-col-reverse md:flex-row m-5"
+          initial={{ y: '100%', opacity: 0 }}
+          animate={{ y: inView ? '0%' : '100%', opacity: inView ? 1 : 0 }}
+          transition={{ duration: 0.7, ease: 'easeIn' }}
+        >
+          <div className="flex flex-col gap-5 p-10 rounded-b-lg lg:rounded-l-lg md:rounded-br-none w-full lg:w-1/4 bg-[#ffff3f]">
+            <div className="flex flex-row justify-center border bg-white text-black rounded-full w-[60px] px-2 py-1">
+              <p>Hello</p>
+            </div>
+
+            <div className="mb-14">
+              <h2 className="text-3xl text-black font-bold">Entry</h2>
+              <p className="text-black">$60</p>
+            </div>
+
+            <p className="text-black">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam,
+              eligendi?
+            </p>
+
+            <Button
+              className="flex flex-row items-center justify-center gap-3 border border-black rounded-full capitalize w-32"
+              variant="outlined"
+            >
+              <p>Signup</p>
+              <ArrowRightIcon className="h-5 w-5" />
+            </Button>
           </div>
 
-          <div className="mb-14">
-            <h2 className="text-3xl text-black font-bold">Entry</h2>
-            <p className="text-black">$60</p>
-          </div>
-
-          <p className="text-black">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam,
-            eligendi?
-          </p>
-
-          <Button
-            className="flex flex-row items-center justify-center gap-3 border border-black rounded-full capitalize w-32"
-            variant="outlined"
-          >
-            <p>Signup</p>
-            <ArrowRightIcon className="h-5 w-5" />
-          </Button>
-        </div>
-
-        <div
-          className="bg-black min-h-[200px] rounded-t-lg lg:rounded-tl-none lg:rounded-r-lg w-full lg:w-3/4 bg-center bg-cover "
-          // bg-opacity-50
-          style={{
-            backgroundImage: "url('/images/billsStadium.jpg')",
-            opacity: 0.7,
-          }}
-        ></div>
-      </motion.div>
+          <div
+            className="bg-black min-h-[200px] rounded-t-lg lg:rounded-tl-none lg:rounded-r-lg w-full lg:w-3/4 bg-center bg-cover "
+            // bg-opacity-50
+            style={{
+              backgroundImage: "url('/images/billsStadium.jpg')",
+              opacity: 0.7,
+            }}
+          ></div>
+        </motion.div>
+      </Link>
     </div>
   );
 };
