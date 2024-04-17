15fed0165c555fe9d36827034e59ed48610c94eb
Jordan Vera <verawebdevelopment@gmail.com>
2024-04-17 00:45:30 -0500

    Update button styles and fix alignment in LoginButton, SignoutButton, and HomepageHero components

--- components/buttons/LoginButton.jsx
+++ components/buttons/LoginButton.jsx
@@ -1,29 +1,16 @@
 const LoginButton = () => {
   return (
-    <button className=" bg-slate-800 no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-full  text-xs font-semibold leading-6  text-white inline-block">
-      <span className="absolute inset-0 overflow-hidden rounded-full">
-        <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100" />
-      </span>
-      <div className="text-black dark:text-white relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-2 px-5 ring-1 ring-gray-900 ">
-        <span>Login</span>
-        <svg
-          fill="none"
-          height="16"
-          viewBox="0 0 24 24"
-          width="16"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M10.75 8.75L14.25 12L10.75 15.25"
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="1.5"
-          />
-        </svg>
-      </div>
-      <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover:opacity-40" />
-    </button>
+    <div class="relative inline-flex  group">
+      <div class="absolute transitiona-all duration-1000 opacity-70 -inset-px bg-gradient-to-r from-[#44BCFF] via-[#FF44EC] to-[#FF675E] rounded-xl blur-lg group-hover:opacity-100 group-hover:-inset-1 group-hover:duration-200 animate-tilt"></div>
+      <button
+        // onClick={() => signOut({ callbackUrl: '/' })}
+        title="Get quote now"
+        class="relative inline-flex items-center justify-center px-8 py-3 text-xs font-bold text-white transition-all duration-200 bg-black font-pj rounded-xl focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900"
+        role="button"
+      >
+        Login
+      </button>
+    </div>
   );
 };
 export default LoginButton;
--- components/buttons/SignoutButton.jsx
+++ components/buttons/SignoutButton.jsx
@@ -2,12 +2,17 @@ import { signOut } from 'next-auth/react';
 
 const SignoutButton = () => {
   return (
-    <button
-      onClick={() => signOut({ callbackUrl: '/' })}
-      className="glow-on-hover px-10 bg-black dark:bg-gray-900 bg-slate-800 no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-full  text-xs font-semibold leading-6  text-white inline-block"
-    >
-      <p className="text-black dark:text-white">Signout</p>
-    </button>
+    <div class="relative inline-flex  group">
+      <div class="absolute transitiona-all duration-1000 opacity-70 -inset-px bg-gradient-to-r from-[#44BCFF] via-[#FF44EC] to-[#FF675E] rounded-xl blur-lg group-hover:opacity-100 group-hover:-inset-1 group-hover:duration-200 animate-tilt"></div>
+      <button
+        onClick={() => signOut({ callbackUrl: '/' })}
+        title="Get quote now"
+        class="relative inline-flex items-center justify-center px-8 py-1 text-xs font-bold text-white transition-all duration-200 bg-black font-pj rounded-xl focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900"
+        role="button"
+      >
+        Signout
+      </button>
+    </div>
   );
 };
 export default SignoutButton;
