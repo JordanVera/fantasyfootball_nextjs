0dd30fc92766d18547e02f1fb4fdbaf7afd0cbcf
Jordan Vera <verawebdevelopment@gmail.com>
2024-05-26 19:36:11 -0500

    refactor: Update login button styling to use consistent colors across components

--- components/forms/LoginForm.jsx
+++ components/forms/LoginForm.jsx
@@ -82,7 +82,7 @@ const LoginForm = ({ setIsSignUp }) => {
 
         <div className="relative group w-full">
           <div className="absolute -inset-0.5 bg-gradient-to-r from-pink-600 to-purple-600 rounded-xl blur opacity-75 group-hover:opacity-100 transition duration-1000 group-hover:duration-200 animate-tilt"></div>
-          <button className="relative px-7 py-4 bg-black rounded-xl leading-none flex items-center divide-x divide-gray-600 w-full  justify-center text-sm font-bold">
+          <button className="relative px-7 py-4 bg-white dark:bg-black text-primary rounded-xl leading-none flex items-center divide-x divide-gray-600 w-full  justify-center text-sm font-bold">
             Sign in
           </button>
         </div>
