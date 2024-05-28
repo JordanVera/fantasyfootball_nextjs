baccf0a8db74a1dbde5656e0a0133051ef5eb083
Jordan Vera <verawebdevelopment@gmail.com>
2024-05-27 23:06:58 -0500

    refactor: Update HomepageHero component UI for improved consistency and user experience

--- components/heros/HomepageHero.jsx
+++ components/heros/HomepageHero.jsx
@@ -1,6 +1,6 @@
 'use client';
 import { motion } from 'framer-motion';
-import { HeroHighlight, Highlight } from '../ui/hero-highlight';
+import { Highlight } from '../ui/hero-highlight';
 import TeamSlider from '../homepage/TeamSlider';
 import { useRegister } from '@/context/RegisterContext';
 
@@ -51,57 +51,67 @@ export function HomepageHero() {
   const { isCollapsed } = useRegister();
 
   return (
-    <HeroHighlight>
-      <div className="flex flex-col gap-32">
-        <div>
-          <motion.h1
-            initial={{
-              opacity: 0,
-              y: 20,
-            }}
-            animate={{
-              opacity: 1,
-              y: [20, -5, 0],
-            }}
-            transition={{
-              duration: 0.5,
-              ease: [0.4, 0.0, 0.2, 1],
-            }}
-            className="text-2xl px-4 md:text-4xl lg:text-5xl font-bold text-black dark:text-white max-w-4xl leading-relaxed lg:leading-snug text-center mx-auto "
-          >
-            Welcome to NFL Last Longer
-          </motion.h1>
-          <motion.h2
-            initial={{
-              opacity: 0,
-              y: 20,
-            }}
-            animate={{
-              opacity: 1,
-              y: [20, -5, 0],
-            }}
-            transition={{
-              duration: 0.5,
-              ease: [0.4, 0.0, 0.2, 1],
-            }}
-            className="text-2xl px-4 md:text-3xl lg:text-3xl font-bold max-w-4xl leading-relaxed lg:leading-snug text-center mx-auto "
-          >
-            <Highlight className="text-lg md:text-3xl text-black dark:text-white">
-              The best NFL Survivor application
-            </Highlight>
-          </motion.h2>
-        </div>
-        <div
-          className={`${
-            !isCollapsed
-              ? 'w-screen lg:w-[calc(100vw-192px)]'
-              : 'w-screen lg:w-[calc(100vw-56px)]'
-          } overflow-hidden`}
+    <div className="relative flex flex-col gap-32 h-[calc(100vh-101px)] items-center justify-center bg-black dark:bg-white rounded-xl m-5 overflow-hidden">
+      <video
+        className="absolute top-0 left-0 w-full h-full object-cover z-0"
+        src="https://www.dropbox.com/s/cb19o9iy8ossfdk/bannerVideo%20copy.mp4?raw=1"
+        type="video/mp4"
+        autoPlay={true}
+        muted={true}
+        loop={true}
+        playsInline={true}
+      />
+
+      <div className="absolute top-0 left-0 w-full h-full bg-black opacity-50 z-10"></div>
+
+      <div className="z-20">
+        <motion.h1
+          initial={{
+            opacity: 0,
+            y: 20,
+          }}
+          animate={{
+            opacity: 1,
+            y: [20, -5, 0],
+          }}
+          transition={{
+            duration: 0.5,
+            ease: [0.4, 0.0, 0.2, 1],
+          }}
+          className="text-2xl px-4 md:text-4xl lg:text-5xl font-bold text-white max-w-4xl leading-relaxed lg:leading-snug text-center mx-auto "
         >
-          <TeamSlider direction="left" teamLogos={teamLogos1} />
-          <TeamSlider direction="right" teamLogos={teamLogos2} />
-        </div>
+          Welcome to NFL Last Longer
+        </motion.h1>
+        <motion.h2
+          initial={{
+            opacity: 0,
+            y: 20,
+          }}
+          animate={{
+            opacity: 1,
+            y: [20, -5, 0],
+          }}
+          transition={{
+            duration: 0.5,
+            ease: [0.4, 0.0, 0.2, 1],
+          }}
+          className="text-2xl px-4 md:text-3xl lg:text-3xl font-bold max-w-4xl leading-relaxed lg:leading-snug text-center mx-auto "
+        >
+          <Highlight className="text-lg md:text-3xl text-white">
+            The Best NFL Survivor Application
+          </Highlight>
+        </motion.h2>
       </div>
-    </HeroHighlight>
+      <div
+        className={`z-20 ${
+          !isCollapsed
+            ? 'w-screen lg:w-[calc(100vw-192px)]'
+            : 'w-screen lg:w-[calc(100vw-56px)]'
+        } overflow-hidden`}
+      >
+        <TeamSlider direction="left" teamLogos={teamLogos1} />
+        <TeamSlider direction="right" teamLogos={teamLogos2} />
+      </div>
+    </div>
   );
 }
