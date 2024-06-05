ef491bfb725380393bd56c9b72bcb49a40f0eb7f
Jordan Vera <verawebdevelopment@gmail.com>
2024-06-04 23:24:50 -0500

    refactor: Add NoBulletsAlert component to PicksDialog
    - Import NoBulletsAlert component in PicksDialog to display an alert when the user has no bullets
    - Conditionally render NoBulletsAlert component based on the user's bullet count

---
+++ components/alerts/NoBulletsAlert.jsx
@@ -0,0 +1,50 @@
+import { Alert, Typography } from '@material-tailwind/react';
+import { useUser } from '@/context/UserContext';
+
+function IconSolid() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      className="h-6 w-6"
+    >
+      <path
+        fillRule="evenodd"
+        d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm8.706-1.442c1.146-.573 2.437.463 2.126 1.706l-.709 2.836.042-.02a.75.75 0 01.67 1.34l-.04.022c-1.147.573-2.438-.463-2.127-1.706l.71-2.836-.042.02a.75.75 0 11-.671-1.34l.041-.022zM12 9a.75.75 0 100-1.5.75.75 0 000 1.5z"
+        clipRule="evenodd"
+      />
+    </svg>
+  );
+}
+
+export default function NoBulletsAlert() {
+  const { setRegistrationOpen, setOpenPicksDialog } = useUser();
+
+  return (
+    <Alert
+      icon={<IconSolid />}
+      className="bg-gradient-to-tl from-orange-600 to-red-500 mb-5"
+    >
+      <h3 className="text-md font-bold">
+        You currently do not have any entries.
+      </h3>
+      <ul className="mt-2 ml-2 list-inside list-disc">
+        <li className="text-sm ">
+          <button
+            onClick={() => {
+              setRegistrationOpen(true);
+              setOpenPicksDialog(false);
+            }}
+            className="underline"
+          >
+            Buy-in to the tournament by clicking here.
+          </button>
+        </li>
+        <li className="text-sm">
+          Once you buy-in come back to this modal to make your selections.
+        </li>
+      </ul>
+    </Alert>
+  );
+}
